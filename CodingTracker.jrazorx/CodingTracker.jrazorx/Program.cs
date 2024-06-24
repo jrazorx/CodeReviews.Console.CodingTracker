e61bacb35c0af38576408deef408305efac3baeb
jra <julien.gault.50@gmail.com>
2024-06-24 16:19:17 +0200

    xml config file to hold the db path

---
+++ CodingTracker.jrazorx/CodingTracker.jrazorx/Program.cs
@@ -0,0 +1,16 @@
+﻿using Spectre.Console;
+using System.Configuration;
+
+class Program
+{
+    static async Task Main(string[] args)
+    {
+        var connectionString = ConfigurationManager.AppSettings.Get("ConnectionString");
+
+        if (string.IsNullOrEmpty(connectionString))
+        {
+            AnsiConsole.MarkupLine("[red]Error: Connection string not found in App.config[/]");
+            return;
+        }
+    }
+}
